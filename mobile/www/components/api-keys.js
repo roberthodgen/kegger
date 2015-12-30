749f40e72f3afe26a1312eaeb322adaa486b1f33
Robert Hodgen <roberthodgen@gmail.com>
2015-12-30 16:31:02 -0500

    Added kegger.api-keys module

---
+++ mobile/www/components/api-keys.js
@@ -0,0 +1,10 @@
+(function () {
+
+	var app = angular.module('kegger.api-keys', []);
+
+	/**
+	 * Careful not to commit API keys!!
+	 */
+	app.constant('BREWERY_DB_API_KEY', '**KEY HERE**');
+
+})();
