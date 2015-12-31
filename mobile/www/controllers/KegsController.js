ab06a78a514a19d40941999ba26d58bf5533697d
Robert Hodgen <roberthodgen@gmail.com>
2015-12-30 23:40:37 -0500

    Loads Kegs from API, displays basic info

--- mobile/www/controllers/KegsController.js
+++ mobile/www/controllers/KegsController.js
@@ -1,12 +1,14 @@
 (function () {
 
-	var app = angular.module('kegger.KegsController', []);
+	var app = angular.module('kegger.KegsController', ['roberthodgen.angular-logger']);
 
-	app.controller('KegsController', ['$scope', function ($scope) {
+	app.controller('KegsController', ['$scope', 'Log', 'Kegs', function ($scope, Log, Kegs) {
 		var self = this;
 
 		$scope.init = function () {
 			Log.info('kegger.KegsController KegsController $scope.init');
+
+			$scope.kegs = Kegs.query();
 		};
 
 		/**
