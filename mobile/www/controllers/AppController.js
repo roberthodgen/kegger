db2e1b0accaa04427d779cdfac6b94673c0b7ed9
Robert Hodgen <roberthodgen@gmail.com>
2015-12-30 23:39:55 -0500

    Loads user via roberthodgen.ndb-users module

--- mobile/www/controllers/AppController.js
+++ mobile/www/controllers/AppController.js
@@ -1,10 +1,23 @@
 (function () {
 
-	var app = angular.module('kegger.AppController', ['roberthodgen.angular-logger', 'ngResource']);
+	var app = angular.module('kegger.AppController', ['roberthodgen.ndb-users', 'roberthodgen.angular-logger', 'ngResource']);
 
-	app.controller('AppController', ['$scope', 'Log', function ($scope, Log) {
+	app.controller('AppController', ['$scope', 'Log', 'Users', function ($scope, Log, Users) {
 		$scope.init = function () {
 			Log.info('kegger.AppController: AppController $scope.init');
+
+			$scope.user = {};
+
+			Users.getCurrentUser().then($scope.userLoggedIn, $scope.userLoginFailure);
+		};
+
+		$scope.userLoggedIn = function (user) {
+			Log.info('kegger.AppController: AppController $scope.userLoggedIn');
+			$scope.user = user;
+		};
+
+		$scope.userLoginFailure = function (data) {
+			debugger;
 		};
 
 
